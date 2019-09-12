ca51ae3642dd921dce1324e8df5da290cdac14b1
github username <github email address>
2019-09-11 20:27:22 -0400

    Attempting to add addusers from; currently datalist is not populating from state

--- src/components/TripDetail.js
+++ src/components/TripDetail.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import Budget from './Budget'
 import LodgingOption from './LodgingOption'
+import AddUserForm from './AddUserForm';
+import LodgingOptionForm from './LodgingOptionForm';
 
 class TripDetail extends React.Component {
     render() {
+
+       
         return( 
             <div className = "columns">
                 <div className = "column is-three-fifths">
-                    {
-                        this.props.trip ? this.props.trip.lodging_options.map(lodging => {
-                return <LodgingOption lodging = {lodging}/>
-            }) : null
-                    }
+                    {/* <LodgingOptionForm trip = {this.props.trip} /> */}
+
+                    {this.props.trip ? this.props.trip.lodging_options.map(lodging => {
+                        return <LodgingOption lodging = {lodging}/>
+                    }) : null }
+
+                    {this.props.trip ?   <AddUserForm trip = {this.props.trip}/> : null }
+
                 </div>
                 <div className = "column">
                     <Budget />
